9c06dcad4fa8955c3a89bf75d8193ebc2f50eea0
sebastianeyra154-sudo <sebastianeyra154@gmail.com>
2026-06-04 09:18:42 -0500

    Create podcasts.js
    podcast

---
+++ data/podcasts.js
@@ -0,0 +1,17 @@
+export const podcastsIndie = [
+  {
+    titulo: "Cine Independiente y Guerrilla",
+    url: https://youtu.be/0q6g7O0C3hE?si=vdeIRC04uDvN6WNa
+    descripcion: "Cómo grabar tu primer cortometraje con poco presupuesto."
+  },
+  {
+    titulo: "Audio para Cineastas Indie",
+    url: https://youtu.be/0DETapou_zk?si=eI4FIF8vJrXmk_eq
+    descripcion: "Técnicas de microfonía y diseño sonoro low-cost."
+  },
+  {
+    titulo: "Guionismo y Producción Audiovisual",
+    url: https://youtu.be/bEX_4Csr2Bg?si=6OOglcCtOSoUpa-m
+    descripcion: "Estructuras narrativas para producciones independientes."
+  }
+];
